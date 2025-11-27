ea1f84a69473d895faa442627c2ce7b249592a91
Ravindu Wanasinghe <ravinduwanasinghe97@gmail.com>
2025-11-27 10:36:08 +0530

    Fix merge conflicts in package.json and postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,12 +1,8 @@
 const config = {
-<<<<<<< HEAD
-  plugins: ["@tailwindcss/postcss"],
-=======
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
->>>>>>> elite
 };
 
 export default config;
